74c9144943c0948f3ff13dd2e671d3204086f7e5
Your Name <jxtsly@gmail.com>
2023-09-30 09:51:58 +0200

    check if password is less than 6 characters

--- controller/authController.go
+++ controller/authController.go
@@ -13,4 +13,12 @@ func Register(c *fiber.Ctx) error {
 	if err := c.BodyParser(&data); err != nil {
 		fmt.Println("Unable to parse body")
 	}
+	// check if password is less than 6 characters
+	if len(data["password"].(string)) <= 6 {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Password must be greater than 6 character",
+		})
+	}
+
 }
