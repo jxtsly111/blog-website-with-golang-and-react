ab437999b52296b3935357b00ad338f5412de101
Your Name <jxtsly@gmail.com>
2023-09-30 10:07:00 +0200

    validateEmail

--- controller/authController.go
+++ controller/authController.go
@@ -2,10 +2,17 @@ package controller
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jxtsly111/blog/models"
 )
+
+func validateEmail(email string) bool {
+	Re := regexp.MustCompile(`[a-z0-9._%+\-]+@[a-z0-9._%+\-]+\.[a-z0-9._%+\-]`)
+	return Re.MatchString(email)
+}
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]interface{}
@@ -18,6 +25,12 @@ func Register(c *fiber.Ctx) error {
 		c.Status(400)
 		return c.JSON(fiber.Map{
 			"message": "Password must be greater than 6 character",
+		})
+	}
+	if !validateEmail(strings.TrimSpace(data["email"].(string))) {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Invalid Email Address",
 		})
 	}
 
