cbe9ae8ed45e0bc753333b2bde46b7d196047607
Your Name <jxtsly@gmail.com>
2023-09-30 10:36:41 +0200

    // check if email already exist in database

--- controller/authController.go
+++ controller/authController.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/jxtsly111/blog/database"
 	"github.com/jxtsly111/blog/models"
 )
 
@@ -33,7 +34,14 @@ func Register(c *fiber.Ctx) error {
 			"message": "Invalid Email Address",
 		})
 	}
-
+	// check if email already exist in database
+	database.DB.Where("email=?", strings.TrimSpace(data["email"].(string))).First(&userData)
+	if userData.Id != 0 {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Email Address already exist",
+		})
+	}
 	user := models.User{
 		FirstName: data["first_name"].(string),
 		LastName:  data["last_name"].(string),
