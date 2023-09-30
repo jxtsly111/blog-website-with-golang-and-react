ad51c1abfc30796cc7c1a0d67af7321f83422e11
Your Name <jxtsly@gmail.com>
2023-09-30 09:44:14 +0200

    golang parse body

--- controller/authController.go
+++ controller/authController.go
@@ -1,1 +1,16 @@
 package controller
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/jxtsly111/blog/models"
+)
+
+func Register(c *fiber.Ctx) error {
+	var data map[string]interface{}
+	var userData models.User
+	if err := c.BodyParser(&data); err != nil {
+		fmt.Println("Unable to parse body")
+	}
+}
