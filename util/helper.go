c795ea7cae60e8585ad11f6b321749ae535aedf1
Your Name <jxtsly@gmail.com>
2023-10-02 19:22:05 +0200

    function for generating jwt

---
+++ util/helper.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+const SecretKey = "secret"
+
+func GenerateJwt(issuer string) (string, error) {
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    issuer,
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	})
+	return claims.SignedString([]byte(SecretKey))
+}
