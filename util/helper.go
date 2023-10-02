a5a70ea1ec69b0355f8a73a3af3f50c0efe548c0
Your Name <jxtsly@gmail.com>
2023-10-02 19:40:59 +0200

    function for parse jwt

--- util/helper.go
+++ util/helper.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"go/token"
 	"time"
 
 	"github.com/golang-jwt/jwt"
@@ -14,4 +15,15 @@ func GenerateJwt(issuer string) (string, error) {
 		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
 	})
 	return claims.SignedString([]byte(SecretKey))
+}
+
+func parsejwt(cookie string) (string, error) {
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+	if err != nil || !token.Valid {
+		return "", err
+	}
+	claims := token.Claims.(*jwt.StandardClaims)
+	return claims.Issuer, nil
 }
